58e504b8cd6da402478b09d4596f8cb30bbdaba1
Andrii Kurdiumov <kant2002@gmail.com>
2018-04-06 18:51:55 +0300

    Reimplement file extraction using boost iostreams

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # scm2txt
-Stacraft maps converter
+Starcraft maps converter
 
 Converts SCM/SCX files to text representation. This text representation allow to be loaded by the testing tools, or other modifications.
+
+## Development
+
+You should have boost iostreams in your search path. You could do that using [VCPkg](http://github.com/Microsoft/vcpkg)
+
+	vcpkg install boost-iostreams
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,65 @@
 #include <iostream>
 #include <string>
+#include <iosfwd>
+#include <boost/iostreams/categories.hpp>
+#include <boost/iostreams/copy.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <boost/iostreams/device/file.hpp>
 #include <StormLib.h>
 
 using namespace std;
+namespace io = boost::iostreams;
 
 const char* SCM_INTERNAL_FILE = "staredit\\scenario.chk";
+
+class mpq_file_source {
+public:
+	typedef char        char_type;
+	typedef io::source_tag  category;
+	
+	mpq_file_source(HANDLE hArchive, const char* fileName)
+		: hFile(NULL)
+	{
+		if (!SFileOpenFileEx(hArchive, fileName, SFILE_OPEN_FROM_MPQ, &hFile))
+		{
+			hFile = NULL;
+		}
+	}
+
+	void close()
+	{
+		if (hFile != NULL)
+		{
+			SFileCloseFile(hFile);
+			hFile = NULL;
+		}
+	}
+
+	bool isValid() const { return hFile != NULL; }
+
+	std::streamsize read(char* s, std::streamsize n)
+	{
+		// Read up to n characters from the underlying data source
+		// into the buffer s, returning the number of characters
+		// read; return -1 to indicate EOF
+		DWORD dwRead = 0;
+		if (!SFileReadFile(hFile, s, n, &dwRead, NULL))
+		{
+			if (dwRead == 0)
+			{
+				return -1;
+			}
+
+			return dwRead;
+		}
+
+		return dwRead;
+	}
+
+	/* Other members */
+private:
+	HANDLE hFile;
+};
 
 void printUsage()
 {
@@ -37,12 +92,17 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
-	if (!SFileExtractFile(hArchive, SCM_INTERNAL_FILE, chkFile.c_str(), SFILE_OPEN_FROM_MPQ))
+	io::stream<mpq_file_source> map(hArchive, SCM_INTERNAL_FILE);
+	if (!map.component()->isValid())
 	{
 		cerr << "Failed to extract the list of files" << endl;
 		SFileCloseArchive(hArchive);
 		return -1;
 	}
+
+	io::stream<io::file_sink> out(chkFile.c_str(), std::ios_base::binary | std::ios_base::trunc | std::ios_base::out);
+	io::copy(map, out);
+	//out.close();
 
 	SFileCloseArchive(hArchive);
 	return 0;
